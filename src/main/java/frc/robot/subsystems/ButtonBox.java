7f0fd5ebc5a1c2791272d2a7a59b58b467c80228
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-07 21:18:42 -0400

    Add a button to the ButtonBox

--- src/main/java/frc/robot/subsystems/ButtonBox.java
+++ src/main/java/frc/robot/subsystems/ButtonBox.java
@@ -4,9 +4,12 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import io.github.roboblazers7617.buttonbox.ButtonBoxServer;
 import io.github.roboblazers7617.buttonbox.controls.TestControl;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.roboblazers7617.buttonbox.controls.Button;
+import io.github.roboblazers7617.buttonbox.controls.Button.ButtonMode;
 
 public class ButtonBox extends SubsystemBase {
 	private final ButtonBoxServer buttonBox;
@@ -14,6 +17,11 @@ public class ButtonBox extends SubsystemBase {
 	/** Creates a new ButtonBox */
 	public ButtonBox() {
 		buttonBox = new ButtonBoxServer();
+
 		buttonBox.addControl(new TestControl());
+
+		Button testButton = new Button();
+		testButton.setMode(ButtonMode.TOGGLE_RISING);
+		buttonBox.addControl(testButton);
 	}
 }
