916f913f84d9594f15422a4a4ec0684095852fa9
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-10 17:19:47 -0400

    Add MIDI functionality to the buttonbox bridge Pretty basic and a lot of the functionality should probably become a part of the library, but it works for now.

--- buttonbox-bridge/src/main/java/io/github/roboblazers7617/tshirtlauncher/buttonbox/ButtonBoxBridge.java
+++ buttonbox-bridge/src/main/java/io/github/roboblazers7617/tshirtlauncher/buttonbox/ButtonBoxBridge.java
@@ -6,6 +6,8 @@ import edu.wpi.first.util.CombinedRuntimeLoader;
 
 import java.io.IOException;
 
+import java.util.Vector;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
@@ -17,14 +19,21 @@ import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.DoubleSubscriber;
 
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiDevice.Info;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+
 import io.github.roboblazers7617.buttonbox.ButtonBoxClient;
-import io.github.roboblazers7617.buttonbox.controls.TestControl;
+import io.github.roboblazers7617.buttonbox.controls.TestControlMIDI;
+import io.github.roboblazers7617.buttonbox.midi.MIDIDevice;
 
 /**
  * Bridge program to connect the TShirtLauncher's ButtonBox hardware to NetworkTables.
  */
 public class ButtonBoxBridge {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, MidiUnavailableException {
 		NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
 		WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
 		WPIMathJNI.Helper.setExtractOnStaticLoad(false);
@@ -38,13 +47,38 @@ public class ButtonBoxBridge {
 		new ButtonBoxBridge().run();
 	}
 
-	public void run() {
+	public void run() throws MidiUnavailableException {
 		NetworkTableInstance inst = NetworkTableInstance.getDefault();
 		inst.startClient4("ButtonBox Bridge");
 		inst.setServer("localhost"); // where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
 		inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
+
+		// Obtain information about all the installed synthesizers.
+		Vector<Info> synthInfos = new Vector<>();
+		MidiDevice device;
+		MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+		for (int i = 0; i < infos.length; i++) {
+			try {
+				device = MidiSystem.getMidiDevice(infos[i]);
+				if (device instanceof Synthesizer) {
+					synthInfos.add(infos[i]);
+				}
+				System.out.printf("%d: %s %s\n", i, infos[i].getClass(), infos[i].getName());
+			} catch (MidiUnavailableException ex) {
+				// Handle or throw exception...
+			}
+		}
+
+		Synthesizer synth = MidiSystem.getSynthesizer();
+		synth.open();
+		MidiDevice rxDevice = MidiSystem.getMidiDevice(infos[3]);
+		MidiDevice txDevice = MidiSystem.getMidiDevice(infos[20]);
+		rxDevice.open();
+		txDevice.open();
+		MIDIDevice midiDevice = new MIDIDevice(rxDevice, txDevice);
+
 		ButtonBoxClient client = new ButtonBoxClient(inst);
-		client.addControl(new TestControl("Test Control"));
+		client.addControl(new TestControlMIDI("Test Control", midiDevice));
 		while (true) {
 			try {
 				Thread.sleep(1000);
